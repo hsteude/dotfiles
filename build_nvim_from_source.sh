53e7410f734161605aee35024141cc27236cc09d
hsteude <henrik.steude@gmail.com>
2022-10-03 14:24:17 +0200

    bug in neovim build script

--- build_nvim_from_source.sh
+++ build_nvim_from_source.sh
@@ -3,5 +3,5 @@ cd neovim
 git checkout stable
 make  CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
-export PATH="$HOME/neovim/bin:$PATH"
+echo 'export PATH="$HOME/neovim/bin:$PATH"' >> .zshrc
 cd ~
