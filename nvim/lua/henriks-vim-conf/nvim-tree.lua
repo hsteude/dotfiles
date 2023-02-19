e84121f3d391300288f7396b7082e5e391729f12
hsteude <henrik.steude@gmail.com>
2023-02-19 17:08:39 +0100

    Remove autocommand for nvim-tree for now

--- nvim/lua/henriks-vim-conf/nvim-tree.lua
+++ nvim/lua/henriks-vim-conf/nvim-tree.lua
@@ -21,9 +21,9 @@ nvim_tree.setup({
 		root_folder_modifier = ":t",
 		icons = {
             show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
+            file = false,
+            folder = false,
+            folder_arrow = false,
             git = true,},
 			glyphs = {
 				default = "",
@@ -39,7 +39,7 @@ nvim_tree.setup({
 					symlink_open = "",
 				},
 				git = {
-					unstaged = "US",
+					unstaged = "U",
 					staged = "S",
 					unmerged = "M",
 					renamed = "➜",
@@ -79,5 +79,5 @@ local function open_nvim_tree()
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
