e7f2ef6b9b78eef9901642ba28247546167c3fed
hsteude <henrik.steude@gmail.com>
2023-03-05 21:59:02 +0200

    black for flake8

--- nvim/lua/henriks-vim-conf/lsp_and_friends.lua
+++ nvim/lua/henriks-vim-conf/lsp_and_friends.lua
@@ -49,8 +49,7 @@ end
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.autopep8,
-		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.prettier,
 	},
 })
