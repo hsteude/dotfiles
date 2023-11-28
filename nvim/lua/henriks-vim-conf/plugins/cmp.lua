169818983ef1534dc7574bff5e505f9de78284d9
hsteude <henrik.steude@gmail.com>
2023-11-28 17:23:50 +0100

    Some new stuff, here and there

--- nvim/lua/henriks-vim-conf/plugins/cmp.lua
+++ nvim/lua/henriks-vim-conf/plugins/cmp.lua
@@ -1,88 +1,97 @@
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
 }
 
 return {
-	"hrsh7th/nvim-cmp",
-	-- load cmp on InsertEnter
-	event = "InsertEnter",
-	-- these dependencies will only be loaded when cmp loads
-	-- dependencies are always lazy-loaded unless specified otherwise
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-	},
-	opts = opts,
-	config = function()
-		local cmp = require("cmp")
-		cmp.setup({
-			mapping = {
-				["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-				["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				-- Accept currently selected item. Set `select` to `false`
-				-- to only confirm explicitly selected items.
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			},
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					-- Kind icons
-					vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-					vim_item.menu = ({
-						nvim_lsp = "[LSP]",
-						buffer = "[Buffer]",
-						path = "[Path]",
-					})[entry.source.name]
-					return vim_item
-				end,
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
-			confirm_opts = {
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
-			},
-			window = {
-				documentation = {
-					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-				},
-			},
-			experimental = {
-				ghost_text = false,
-				native_menu = false,
-			},
-		})
-	end,
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+    },
+    opts = opts,
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+            mapping = {
+                ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+                ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                -- Accept currently selected item. Set `select` to `false`
+                -- to only confirm explicitly selected items.
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            },
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    -- Kind icons
+                    vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                    vim_item.menu = ({
+                        nvim_lsp = "[LSP]",
+                        buffer = "[Buffer]",
+                        path = "[Path]",
+                    })[entry.source.name]
+                    return vim_item
+                end,
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+                {
+                    name = "spell",
+                    option = {
+                        keep_all_entries = false,
+                        enable_in_context = function()
+                            return true
+                        end,
+                    },
+                },
+            },
+            confirm_opts = {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = false,
+            },
+            window = {
+                documentation = {
+                    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+                },
+            },
+            experimental = {
+                ghost_text = false,
+                native_menu = false,
+            },
+        })
+    end,
 }
