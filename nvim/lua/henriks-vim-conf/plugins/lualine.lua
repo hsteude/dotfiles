fe88775e53b89301a17c6b382d30a58ea7e42fd6
hsteude <henrik.steude@gmail.com>
2024-01-14 19:20:13 +0100

    Added lualine plugin

---
+++ nvim/lua/henriks-vim-conf/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "auto",
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = {},
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+        })
+    end,
+}
