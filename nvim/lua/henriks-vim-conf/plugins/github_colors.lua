8b20674723b9af58567e665504c5b8534c701b5d
hsteude <henrik.steude@prokube.ai>
2024-08-08 07:29:58 +0200

    Installed github colors

--- nvim/lua/henriks-vim-conf/plugins/github_colors.lua
+++ nvim/lua/henriks-vim-conf/plugins/github_colors.lua
@@ -4,6 +4,5 @@ return {
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		require("github-theme").setup({})
-		vim.cmd("colorscheme github_light")
 	end,
 }
