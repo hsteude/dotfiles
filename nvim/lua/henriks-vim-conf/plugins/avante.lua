ad9f6a9ce265c9afd17ba2e53988ee7885a116e8
hsteude <henrik.steude@prokube.ai>
2024-12-10 15:46:11 +0100

    Back to default model for avante (antropic)

--- nvim/lua/henriks-vim-conf/plugins/avante.lua
+++ nvim/lua/henriks-vim-conf/plugins/avante.lua
@@ -4,10 +4,10 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-        provider = 'openai',
-        openai = {
-            model = "gpt-4o-mini"
-        },
+        -- provider = 'openai',
+        -- openai = {
+        --     model = "gpt-4o-mini"
+        -- },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
